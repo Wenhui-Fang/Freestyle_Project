1707f97cb02ce7384bfb341a81100e57fbe02fdc
Wenhui Fang <46543296+windy030@users.noreply.github.com>
2019-04-28 23:10:32 -0400

    Read GUI Input

--- webRequest.py
+++ webRequest.py
@@ -7,20 +7,21 @@ import re
 import PySimpleGUI as sg  
 
 
-
 #adapted from https://pypi.org/project/PySimpleGUI/
 
 layout = [[sg.Text('Welcome to use the App', size=(30, 1), font=("Helvetica", 25), text_color='blue')],      
    [sg.Text('Please enter the zip code where you will work or go to college')],      
-   [sg.Text('zipcode', size=(15, 1)), sg.InputText('20057', key='_zipcode_')],     
-   [sg.Checkbox('Studio'), sg.Checkbox('My second checkbox!', default=True)],      
-   [sg.Radio('My first Radio!     ', "RADIO1", default=True), sg.Radio('My second Radio!', "RADIO1")],      
-   [sg.Multiline(default_text='This is the default Text shoulsd you decide not to type anything',)],      
-[sg.InputCombo(['Combobox 1', 'Combobox 2'], size=(20, 3)),      
- sg.Slider(range=(1, 100), orientation='h', size=(35, 20), default_value=85)],      
-[sg.Listbox(values=['Listbox 1', 'Listbox 2', 'Listbox 3'], size=(30, 6))],       
+   [sg.Text('zipcode', size=(15, 1)), sg.InputText('20057', key='_zipcode_')],
+   [sg.Text('Please determine how many miles you are willing to commute, and your desired room plan.',)],       
+   [sg.Checkbox('< 10 miles', size=(10,1)),  sg.Checkbox('10 - 30 miles', default=True)],
+   [sg.Radio('I want a studio!     ', "RADIO1", default=True, size=(10,1)), sg.Radio('I want 1B1B!', "RADIO1")],      
+[sg.Text('My Monthly Budget is:', size=(18, 1))],    
+[sg.InputCombo(['<= $1,000', '> $1,000'], size=(20, 3))],      
 [sg.Submit(), sg.Cancel()]]    
-event, values  = sg.Window('Everything bagel', auto_size_text=True, default_element_size=(40, 1)).Layout(layout).Read()    
+event, values  = sg.Window('OPIM 242 FreeStyle Project', auto_size_text=True, default_element_size=(40, 1)).Layout(layout).Read()    
+
+print(values)
+# {'_zipcode_': '20057', 0: True, 1: True, 2: True, 3: False, 4: '<= $1,000'}
 
 
 Test_Zipcode = values['_zipcode_']  
@@ -74,114 +75,3 @@ number_of_listing = len(housing_listing_1b)
 
 for i in range(0, 6):
     print(housing_listing_1b[0]["values"][i]["x"] + ":             " + str(housing_listing_1b[0]["values"][i]["y"]))
-
-
-# city_no_space = re.sub(' ','',matching_city)
-
-
-
-# test = nearby_neighborhoods[0]
-# test.major_city
-
-
-# # print(type(zipcode))
-
-# breakpoint()
-
-# # request_url = ("https://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz17zh9n8ivpn_7j3qs&state=wa&city=seattle&childtype=neighborhood")
-# # request_url = ("https://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz17zh9n8ivpn_7j3qs&state=ny&city=newyork&childtype=neighborhood")
-# request_url = ("https://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz17zh9n8ivpn_7j3qs&state=ny&city=newyork&childtype=zipcode")
-
-# response = requests.get(request_url)
-
-
-# print(response)
-# print(type(response))
-# contents = response.text
-# print(type(contents))
-
-# soup = BeautifulSoup(contents,'xml')
-
-# # print(soup)
-# print(type(soup))
-
-
-
-# soup.find("response").find_all("list")
-# Response_list = soup.find("response").find("list")
-# price = soup.find("response").find("list").find("region").find_all("zindex")
-
-
-
-# regions = soup.find("response").find("list").find_all("region")
-
-# for i in range(0,len(regions)):
-#     # print(regions[i].find("zindex").text)
-#     price = regions[i].find("zindex")
-#     if price:
-#         print(price.text)
-#     else:
-#         print("No price available for this region.")
-
-
-breakpoint()
-
-# http://www2.hawaii.edu/~takebaya/cent110/xml_parse/xml_parse.html
-# from bs4 import BeautifulSoup
-# infile = open("books.xml","r")
-# contents = infile.read()
-# soup = BeautifulSoup(contents,'xml')
-# titles = soup.find_all('title')
-# authors = soup.find_all('author')
-# prices = soup.find_all('price')
-# for i in range(0, len(titles)):
-#     print(titles[i].get_text(),"by",end=' ')
-#     print(authors[i].get_text(),end=' ')
-#     print("costs $" + prices[i].get_text())
-
-
-
-# for i < len(price):
-#     Price_list.append(price[0].text)
-
-
-
-
-
-
-# zillow_api = "https://www.zillow.com/webservice/GetRegionChildren.htm?zws-id=X1-ZWz17zh9n8ivpn_7j3qs&state=wa&city=seattle&childtype=neighborhood"
-
-# try:
-#     r = requests.head(zillow_api)
-#     print(r.status_code)
-
-#     if (r.status_code) == 200:
-#         print(type(r))
-#         print(r)
-
-#         # where do I specify xml or json?
-
-#     # prints the int of the status code. Find more at httpstatusrappers.com :)
-# except requests.ConnectionError:
-#     print("failed to connect")
-
-
-
-# import numpy as np
-# import json
-# import os
-# import requests
-
-# # api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
-
-
-#   # making a request after successful data validation
-# request_url = ('http://api.indeed.com/ads/apisearch?publisher=123412341234123&q=java+developer&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2')
-# response = requests.get(request_url)
-
-# # type(response)
-
-# # parsed_response = json.loads(response.text)
-
-# # print(parsed_response)
-# # type(parsed_response)
