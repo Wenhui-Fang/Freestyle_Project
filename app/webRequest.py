17c0e6ed45ea7ec670121ee57df3587d07195759
Wenhui Fang <46543296+windy030@users.noreply.github.com>
2019-05-03 10:51:01 -0400

    Back to Command Line for now

--- app/webRequest.py
+++ app/webRequest.py
@@ -6,7 +6,7 @@ import zipcodes
 
 dashline = "____________________________________"
 
-#Simpe GUI adapted from https://pypi.org/project/PySimpleGUI/
+#Simpe GUI adapted from https://pypi.org/project/PySimpleGUI/, allowing the users to set their preferences
 layout = [[sg.Text('Welcome to use Tell-Me-Where-To-Live', size=(30, 1), font=("Helvetica", 25), text_color='blue')],      
    [sg.Text('Please enter the zip code where you will work or go to college')],      
    [sg.Text('zipcode', size=(15, 1)), sg.InputText('20057', key='_zipcode_')],
@@ -21,9 +21,7 @@ event, values  = sg.Window('OPIM 242 FreeStyle Project', auto_size_text=True, de
 # print(values)
 # {'_zipcode_': '20057', 0: True, 1: True, 2: True, 3: False, 4: '<= $1,000'}
 
-# breakpoint()
-
-#Input validation - Zip Code
+#Input validation using zipcodes package - Zipcode; terminates the application if wrong input is captured
 try:
          zipcodes.matching(values['_zipcode_'])
 except: 
@@ -42,51 +40,49 @@ else:
         sg.Popup("Warning!","You must select miles you are willing to commute. The application will terminate now. Please start again...")
         sys.exit()
 
-user_zipcode = values['_zipcode_']  
-
-#Read users' preferred housing;
+#Read the users' preferred housing;
 if values[2] == True:
         housing_style = "studio"
 elif values[3] == True:
         housing_style = "1b1b"
 
-#Read users' budget
+#Read the users' budget
 if values[4] == "<= $1,000":
         budget = "less than $1,000."
 elif values[4] == "> $1,000":
         budget = "more than $1,000."
 
+#Input Validation complete, start using built-in functions within uszipcode package to search data
+user_zipcode = values['_zipcode_']  
 search = SearchEngine(simple_zipcode=False)
 zipcode = search.by_zipcode(user_zipcode)
-
-# breakpoint()
 
 #converting data to dictionary
 matching_zip = zipcode.to_dict()
 
-#Some zip code are PO Box which requires validation
+#Some zip code are associated with PO Box which requires a different way of presenting location
 if matching_zip["post_office_city"] != "None":
         matching_city = matching_zip["post_office_city"]
 else:
         matching_city = (matching_zip["county"] + ", " + matching_zip['state'])
 
-matching_state = matching_zip["state"]
-
+#Let the users know their input with a pop-up window
 sg.Popup('Input Verification',"So you will be studying or working in " + matching_city + ". And you are willing to commute up to " + 
 str(miles_to_commute) + " miles. You want a " + housing_style + " and your budget is " + budget + ". Click Ok to Proceed.")
 
+#using built-in data within uszipcode package to determine latitude and longtitude of the target zipcode/location
 latitude = matching_zip["lat"]
 longtitude = matching_zip["lng"]
 
-miles_to_commute = 30
-
+#using built-in functions within uszipcode package to search nearby neighborhodds, setting maximum 10 neighborhoods for now
 nearby_neighborhoods = search.by_coordinates(latitude, longtitude, radius=int(miles_to_commute),returns=10)
 
 where_to_live = []
 housing_listing = []
 print("Below is a list of neighborhoods you can consider: \n")
 
-for i in range(0,5):
+#searching 5
+for i in range(0,6):
     city = nearby_neighborhoods[i].post_office_city
     if city not in where_to_live:
         where_to_live.append(city)
@@ -103,32 +99,16 @@ print("Price Range: " + "       " + "Number of listings: ")
 print(dashline)
 number_of_listing = len(housing_listing)
 
-# for i in range(0, 6):
-#     print((housing_listing[0]["values"][i]["x"]).ljust(10) + ":"+ (str(housing_listing[0]["values"][i]["y"]).rjust(25)))
+for i in range(0, 6):
+    print((housing_listing[0]["values"][i]["x"]).ljust(10) + ":"+ (str(housing_listing[0]["values"][i]["y"]).rjust(25)))
 
+# breakpoint()
 
-sg.Popup("Below is a list of neighborhoods you can consider:", where_to_live)
+# sg.Popup("Below is a list of neighborhoods you can consider:", where_to_live)
 
 # print=sg.Print  
-for i in range(0, 6):
-        sg.EasyPrint((housing_listing[0]["values"][i]["x"]).ljust(10) + ":"+ (str(housing_listing[0]["values"][i]["y"]).rjust(25)))
+# for i in range(0, 6):
+#         sg.Print((housing_listing[0]["values"][i]["x"]).ljust(10) + ":"+ (str(housing_listing[0]["values"][i]["y"]).rjust(25)))
 
-
-# window_rows = [[sg.Text('SHA-1 and SHA-256 Hashes for the file')],      
-#                  [sg.InputText(), sg.FileBrowse()],      
-#                  [sg.Submit(), sg.Cancel()]]      
-
-# window = sg.Window('SHA-1 & 256 Hash', window_rows)    
-
-# event, values = window.Read()   
-
-# window = sg.Window('My window title', layout)   
-# window.Read
-
-# window = sg.Window('My PySimpleGUIWeb Window',
-#                   layout=layout,
-#                    default_element_size=(12,1),
-#                    font='Helvetica 18',
-#                    )
-
-sg.PopupScrolled(print("Price Range: " + "       " + "Number of listings: "))
+# #to hold above print list
+# sg.PopupScrolled(print("Price Range: " + "       " + "Number of listings: "))
